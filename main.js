9c2f58e26e090e79d25b54bb156ad5032baa4a64
CoffeeIsCode <112358010+CoffeeIsCode@users.noreply.github.com>
2022-10-17 23:39:23 -0400

    Create main.js
    Started working on another way this game could be done.

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+
+main.js
+This is going to be an additional file used for a different style of this game.
+
+*/
+
+let player = {};
+player.pos = {};
+player.pos.x = 5;
+player.pos.y = 5;
+
+player.screen = {};
+player.screen.x = 5;
+player.screen.y = 5;
+player.screen.xMax = 10;
+player.screen.yMax = 10;
+player.max = {};
+player.max.x = [];
+player.max.y = [];
+
+let block = {};
+block.id = [];
+block.type = [];
+block.pos = {};
+block.x = [];
+block.y = [];
+
+
+let map = {};
+map.x = [];
+map.y = [];
+map.block = [];
+map.id = [];
+
+map.id[0] = "new grounds";
+map.x[0] = true;
+map.y[0] = true;
+
+
+
+function movePlayer(direction) {
+  
+}
+
+function checkPlayerMove() {
+}
