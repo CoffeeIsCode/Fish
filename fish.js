e4558df2eed4cff722b9797e2e97397276d5da7a
CoffeeIsCode <oneweekendwarrior@gmail.com>
2023-12-16 04:20:31 -0500

    Minor Update

--- fish.js
+++ fish.js
@@ -2,8 +2,55 @@
 fish.js
 */
 
-var elm = {};
+var player = {};
+var p = player;
+p.xp = {};
+p.xp.targets = "100","200","400","1200","3200","8000";
+
+function startPlayer() {
+  p.hp = 100;
+  p.sp = 100;
+  p.x = 333;
+  p.y = 333;
+  p.direction = "";
+  p.isMoving = false;
+  p.isAlive = false;
+  p.lvl = 0;
+}
+var fish = player;
+var f = fish;
+
+
+
+var world = {
+  map: 1,
+  min: { x: 0, y: 0 },
+  max: { x: 1000, y: 1000 },
+};
+var w = world;
+
+function playerDirection(dir) {
+    if (dir == "up") {}
+    if (dir == "right") {}
+    if (dir == "down") {}
+    if (dir == "left") {}
+}
+
+function getInput(e) {
+    console.log(e);
+}
+
+document.addEventListener(onkeydown,getInput(e));
+document.addEventListener(onkeyup,getKeyUp(e));
+
+var block = {};
+var b = block;
+
+function startWorld() {}
+
+var elm = {}; // create 'elm' to store reused document elements.
 elm.player = document.getElementById("player");
+elm.p = elm.player;
 elm.score = document.getElementById("score");
 elm.play = document.getElementById("play");
 elm.title = document.getElementById("title");
@@ -18,8 +65,12 @@ function playButton() {
   elm.subtitle.style.visibility = "hidden";
   elm.canvas.style.visibility = "visible";
   elm.play.style.visibility = "hidden";
-  elm.canvas.style.visibility = "wisible";
+  elm.canvas.style.visibility = "visible";
   elm.player.style.visibility = "visible";
+}
+
+function fishMoveRandom() {
+
 }
 
 function fishRight() {}
